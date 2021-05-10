6dd0fea831cd326d5c26a147185249679934d5e8
Konan Houphoue <bahobab@hotmail.com>
2021-05-10 18:00:31 -0500

    implemented http-proxy-middleware

--- packages/local-api/dist/index.d.ts
+++ packages/local-api/dist/index.d.ts
@@ -1,1 +1,1 @@
-export declare const serve: (port: string, filename: string, dir: string) => Promise<void>;
+export declare const serve: (port: string, filename: string, dir: string, useProxy: boolean) => Promise<void>;
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,10 +1,24 @@
 import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path';
 
-export const serve = (port: string, filename: string, dir: string) => {
+export const serve = (port: string, filename: string, dir: string, useProxy: boolean) => {
+
   const app = express();
+
+  
+  if (useProxy) {
+    app.use(createProxyMiddleware({
+      target: 'http://localhost:3000',
+      ws: true,
+      logLevel: 'silent',
+    }));
+  } else {
+    const packagePath = path.resolve('local-client/build/index.html');
+    app.use(express.static(path.dirname(packagePath)));
+  }
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
   });
-
 }
