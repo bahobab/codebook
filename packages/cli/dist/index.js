17f0127cff5b64a2dfdf703909f1f0fa07159af3
Konan Houphoue <bahobab@hotmail.com>
2021-05-10 14:54:17 -0500

    implemented dev packages with lerna. using typescript

---
+++ packages/cli/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var local_api_1 = require("local-api");
+local_api_1.serve();
---
+++ packages/local-api/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.serve = void 0;
+var serve = function () {
+    console.log('server is running...!');
+};
+exports.serve = serve;
